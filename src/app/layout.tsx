433d40002c640dd48b60741a907ba1ca321e2d6f
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-11-06 15:21:58 +0200

    add breakpoints ....

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import LeftBar from "./components/LeftBar";
+import RightBar from "./components/RightBar";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +9,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between ">
+          <div className="px-2 xsm:px-4 xxl:px-8 bg-red-200 h-screen ">< LeftBar /></div>
+          <div className="bg-green-200 lg:min-w-[600px] border-x-[1px] border-yellow-500 h-screen flex-1">{children}</div>
+          <div className="hidden lg:flex bg-blue-200 h-screen "><RightBar /></div>
+
+        </div>
+      </body>
     </html>
   );
 }
