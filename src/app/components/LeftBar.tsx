39a79f4616ea3113ff08b3aa862ded24cc26468a
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-11-09 11:53:34 -0800

    design letfbar

--- src/app/components/LeftBar.tsx
+++ src/app/components/LeftBar.tsx
@@ -1,9 +1,96 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
-
+const menuList = [
+    {
+        id: 1,
+        name: "Homepage",
+        link: "/",
+        icon: "home.svg",
+    },
+    {
+        id: 2,
+        name: "Explore",
+        link: "/",
+        icon: "explore.svg",
+    },
+    {
+        id: 3,
+        name: "Notification",
+        link: "/",
+        icon: "notification.svg",
+    },
+    {
+        id: 4,
+        name: "Messages",
+        link: "/",
+        icon: "message.svg",
+    },
+    {
+        id: 5,
+        name: "Bookmarks",
+        link: "/",
+        icon: "bookmark.svg",
+    },
+    {
+        id: 6,
+        name: "Jobs",
+        link: "/",
+        icon: "job.svg",
+    },
+    {
+        id: 7,
+        name: "Communities",
+        link: "/",
+        icon: "community.svg",
+    },
+    {
+        id: 8,
+        name: "Premium",
+        link: "/",
+        icon: "logo.svg",
+    },
+    {
+        id: 9,
+        name: "Profile",
+        link: "/",
+        icon: "profile.svg",
+    },
+    {
+        id: 10,
+        name: "More",
+        link: "/",
+        icon: "more.svg",
+    },
+];
 export default function LeftBar() {
     return (
-        <div>
-            LeftBar
+        <div className=' h-screen  flex justify-between sticky top-0 flex-col pt-2 pb-8'>
+            {/* logo,menu&button  */}
+            <div className='flex flex-col gap-4 text-lg items-center xxl:items-start'>
+                {/* logo  */}
+                <Link href='/' className='p-2 rounded-full hover:bg-[#181818]'>
+                    <Image src='icons/logo.svg' alt='logo ' width={24} height={24} />
+                </Link>
+                {/* menu  */}
+                <div className='flex flex-col gap-4'>
+                    {
+                        menuList.map(item => (
+                            <Link href={item.link} key={item.id} className='p-2 rounded-full hover:bg-[#181818] flex items-center gap-4'>
+                                <Image src={`icons/${item.icon}`} alt={item.name}
+                                    width={24} height={24} />
+                                <span className='hidden xxl:inline'>{item.name}</span>
+                            </Link>
+                        ))
+                    }
+                </div>
+                {/* button  */}
+                
+            </div>
+            {/* user  */}
+            <div>
+
+            </div>
         </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between ">
-          <div className="px-2 xsm:px-4 xxl:px-8 bg-red-200 h-screen ">< LeftBar /></div>
+          <div className="px-2 xsm:px-4 xxl:px-8 ">< LeftBar /></div>
           <div className="bg-green-200 lg:min-w-[600px] border-x-[1px] border-yellow-500 h-screen flex-1">{children}</div>
           <div className="hidden lg:flex bg-blue-200 h-screen "><RightBar /></div>
 
